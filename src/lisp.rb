e2aa90cf25f34c76d285940e8c3dfb1b2b499523
Jarrett Baugh <jarrett.baugh@gmail.com>
2018-09-28 22:00:10 -0700

    Preliminary support for quote (not entirely sure of use for it yet)

--- src/lisp.rb
+++ src/lisp.rb
@@ -1,13 +1,15 @@
 require_relative 'environment'
 
 class Lisp
-  def initialize(env)
+  def initialize(env = {})
     @env = Environment.new(env)
   end
 
   def eval(expr, env = @env)
     if expr.is_a? Numeric
       expr
+    elsif expr[0] == :quote
+      expr[1..-1]
     end
   end
 end
--- test/lisp_test.rb
+++ test/lisp_test.rb
@@ -4,6 +4,11 @@ require_relative '../src/lisp'
 class LispTest < Minitest::Test
   def test_lisp_numeric
     ast = 1
-    assert_equal Lisp.new({}).eval(ast), 1
+    assert_equal Lisp.new.eval(ast), 1
+  end
+
+  def test_lisp_quote
+    ast = [:quote, 1]
+    assert_equal Lisp.new.eval(ast), [1]
   end
 end
