79e934a4e61f5e548ea22a95f5630007187e9949
Jarrett Baugh <jarrett.baugh@gmail.com>
2018-09-28 19:31:01 -0700

    Improve environment to mimic hash, but abstract away implementation

--- src/environment.rb
+++ src/environment.rb
@@ -3,11 +3,21 @@ class Environment
 
   def initialize(extra = {})
     @env = {
-
+      car:    lambda { |*list| list[0] },
+      cdr:    lambda { |*list| list.drop 1 },
+      cons:   lambda { |(e, cell), _| [e] + cell }
     }.merge(extra)
   end
 
-  def read(key)
+  def get(key)
     @env[key]
+  end
+
+  def set(key, val)
+    @env[key] = val
+  end
+
+  def merge(opts = {})
+    @env.merge!(opts)
   end
 end
--- test/environment_test.rb
+++ test/environment_test.rb
@@ -4,11 +4,25 @@ require_relative '../src/environment'
 class EnvironmentTest < Minitest::Test
   def test_environment_extends_passed_in
     opts = { foo: 'a' }
-    assert_equal Environment.new(opts).read(:foo), 'a'
+    assert_equal Environment.new(opts).get(:foo), 'a'
   end
 
   def test_environment_read_on_invalid_key
     opts = { foo: 'a' }
-    assert_nil Environment.new(opts).read(:bar)
+    assert_nil Environment.new(opts).get(:bar)
+  end
+
+  def test_environment_set
+    env = Environment.new({})
+    env.set(:foo, 'a')
+    assert_equal env.get(:foo), 'a'
+  end
+
+  def test_environment_merge
+    env = Environment.new({})
+    opts = { foo: 'a', bar: 'b' }
+    env.merge(opts)
+    assert_equal env.get(:foo), 'a'
+    assert_equal env.get(:bar), 'b'
   end
 end
