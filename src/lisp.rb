b7a89653d75c9311dfed6f15486dc33e07c025c5
Jarrett Baugh <jarrett.baugh@gmail.com>
2018-09-28 22:53:17 -0700

    Add spaces for eval method, to be implemented later

--- src/lisp.rb
+++ src/lisp.rb
@@ -8,8 +8,18 @@ class Lisp
   def eval(expr, env = @env)
     if expr.is_a? Numeric
       expr
+    elsif expr.is_a? Symbol
+      env.get(expr)
     elsif expr[0] == :quote
       expr[1..-1]
+    elsif expr[0] == :if
+      
+    elsif expr[0] == :define
+      
+    elsif expr[0] == :lambda
+      
+    else
+      
     end
   end
 end
