e7dd8a16f917e75c24137549ae464c4bf33f9741
Jarrett Baugh <jarrett.baugh@gmail.com>
2018-09-29 00:07:44 -0700

    Pass environment into eval for evaluator (separate concerns into vm)

--- src/evaluator.rb
+++ src/evaluator.rb
@@ -1,38 +1,34 @@
 require_relative 'environment'
 
 class Evaluator
-  def initialize(env = {})
-    @env = Environment.new(env)
-  end
-
-  def eval(expr)
+  def eval(env, expr)
     if expr.is_a? Numeric
       expr
     elsif expr.is_a? Symbol
-      @env.get(expr)
+      env.get(expr)
     elsif expr[0] == :quote
       expr[1..-1]
     elsif expr[0] == :if
       # Gets the test condition, the if-true expression, and the if-false expression
       _, test, conseq, alt = expr
       # Tests the condition, and gets the next code to execute based on condition
-      expr = eval(test) ? conseq : alt
+      expr = eval(env, test) ? conseq : alt
       # Executes the correct code path
-      eval(expr)
+      eval(env, expr)
     elsif expr[0] == :define
       _, var, e = expr
       # Adds the evaluated expression identified by var to the environment
-      @env.set(var, eval(e))
+      env.set(var, eval(env, e))
     elsif expr[0] == :lambda
       _, params, e = expr
       # Evaluates the lambda in the context of this environment
-      @env.merge(Hash[params.zip(args)])
-      lambda { |*args| self.eval(e) }
+      env.merge(Hash[params.zip(args)])
+      lambda { |*args| self.eval(env, e) }
     else
       # Handles a list expression
-      code = eval(expr[0])
+      code = eval(env, expr[0])
       # Gets remaining arguments, and evaluates them
-      args = expr[1..-1].map{ |arg| eval(arg) }
+      args = expr[1..-1].map{ |arg| eval(env, arg) }
       # Calls function found in code, passing in evaluated arguments
       code.(*args)
     end
--- test/evaluator_test.rb
+++ test/evaluator_test.rb
@@ -1,32 +1,34 @@
 require 'minitest/autorun'
 require_relative '../src/evaluator'
+require_relative '../src/environment'
 
 class EvaluatorTest < Minitest::Test
   def test_evaluator_numeric
     ast = 1
-    assert_equal Evaluator.new.eval(ast), 1
+    assert_equal Evaluator.new.eval(Environment.new, ast), 1
   end
 
   def test_evaluator_quote
     ast = [:quote, 1]
-    assert_equal Evaluator.new.eval(ast), [1]
+    assert_equal Evaluator.new.eval(Environment.new, ast), [1]
   end
 
   def test_evaluator_if
     ast = [:if, [:<, 1, 2], 1, 2]
-    assert_equal Evaluator.new.eval(ast), 1
+    assert_equal Evaluator.new.eval(Environment.new, ast), 1
   end
 
   def test_evaluator_addition
     ast = [:+, 1, 2, 3, 4]
-    assert_equal Evaluator.new.eval(ast), 10
+    assert_equal Evaluator.new.eval(Environment.new, ast), 10
   end
 
   def test_evaluator_define
     asts = [[:define, :r, 10], [:define, :pi, 3.14], [:*, :pi, [:*, :r, :r]]]
     evaluator = Evaluator.new
+    env = Environment.new
     asts.each do |ast|
-      evaluator.eval(ast)
+      evaluator.eval(env, ast)
     end
   end
 end
